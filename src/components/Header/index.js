59fb60da4e97f24eec5bbab240bdf982095ce3f0
Luis Silva <luisg.juliao@gmail.com>
2021-07-18 15:37:15 -0300

    module fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
